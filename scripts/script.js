0fe2b9be11eb6bf5a0d8056c0fb620a8ae4b1efe
bfjy2024 <slwu19@163.com>
2024-10-15 21:45:13 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,7 @@ new Vue({
           name: "年少有为",
           artist: "李荣浩",
           cover: "https://y.qq.com/music/photo_new/T002R800x800M000004QnEHc3zjC7J_1.jpg?max_age=2963246343",
-          sourse: "https://sjy6.stream.qqmusic.qq.com/F000000KFNNA0BDekK.flac?guid=www.hhlqilongzhu.cn&vkey=0EB36A8DD5C113C3AD12F71AB75255BCEC9019C7775E589CCEA89D6280D5549A5AC55A1B77D8B4FE39B96C64A68F509803B0AFECCECA6501&uin=2037377155&fromtag=5201314&info=cache&from=longzhu_api",
+          sourse: "https://m701.music.126.net/20241015220736/4fd9b3883243cc291714a20869d961e2/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/32283600345/f29b/d599/f294/a7361dca69f85d80e3c027f2ec0550cd.mp3?from=longzhu_api",
           url: "https://i.y.qq.com/v8/playsong.html?songmid=004DXFlC0nsTCZ&type=0",
         },
         {
@@ -218,7 +218,7 @@ new Vue({
           name: "年少有为",
           artist: "李荣浩",
           cover: "https://y.qq.com/music/photo_new/T002R800x800M000004QnEHc3zjC7J_1.jpg?max_age=2963246343",
-          sourse: "https://sjy6.stream.qqmusic.qq.com/F000000KFNNA0BDekK.flac?guid=www.hhlqilongzhu.cn&vkey=0EB36A8DD5C113C3AD12F71AB75255BCEC9019C7775E589CCEA89D6280D5549A5AC55A1B77D8B4FE39B96C64A68F509803B0AFECCECA6501&uin=2037377155&fromtag=5201314&info=cache&from=longzhu_api",
+          sourse: "https://m701.music.126.net/20241015220736/4fd9b3883243cc291714a20869d961e2/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/32283600345/f29b/d599/f294/a7361dca69f85d80e3c027f2ec0550cd.mp3?from=longzhu_api",
           url: "https://i.y.qq.com/v8/playsong.html?songmid=004DXFlC0nsTCZ&type=0",
           favorited: false,
         },
